dbac54db6cc864965a3cb4ccedd601659fd1b0de
Henry.T <tang.hy@live.com>
2016-07-26 16:03:53 +0800

    Template

---
+++ Misc/Template/src/com/lolofinil/Misc/Template/MainActivity.java
@@ -0,0 +1,22 @@
+package com.lolofinil.AndroidPG.Misc.Tempate;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    public static String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
